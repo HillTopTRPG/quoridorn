7da9900a9229938f801258b8e165205d10fe735b
HillTopTRPG <mihikari90@gmail.com>
2018-09-25 10:15:36 +0900

    右クリックで回転できるようにした

--- src/components/map/GameTable.vue
+++ src/components/map/GameTable.vue
@@ -4,7 +4,8 @@
     dropzone="move"
     @dragover.prevent
     @drop.prevent="drop"
-    :style="gameTableStyle">
+    :style="gameTableStyle"
+    @contextmenu.prevent>
 
     <div class="mapBoardFrame"
       @mousedown.left.prevent="leftDown" @mouseup.left.prevent="leftUp"
@@ -31,27 +32,33 @@ export default {
   data () {
     return {
       isDraggingLeft: false,
-      leftMove: {
-        totalX: 0,
-        totalY: 0,
-        draggingX: 0,
-        draggingY: 0
-      },
       isDraggingRight: false,
-      rightMove: {
-        totalX: 0,
-        totalY: 0,
-        draggingX: 0,
-        draggingY: 0
-      },
       mouse: {
         x: 0,
         y: 0,
-        saveX: 0,
-        saveY: 0
+        left: {
+          from: { x: 0, y: 0 },
+          to: { x: 0, y: 0 },
+          totalX: 0,
+          totalY: 0,
+          draggingX: 0,
+          draggingY: 0
+        },
+        right: {
+          from: { x: 0, y: 0 },
+          to: { x: 0, y: 0 },
+          totalX: 0,
+          totalY: 0,
+          draggingX: 0,
+          draggingY: 0
+        }
+      },
+      angle: {
+        total: 0,
+        dragging: 0,
+        dragStart: 0
       },
       translateZ: 0,
-      rotateZ: 0,
       rotateY: 0,
       rotateX: 0
     }
@@ -79,22 +86,7 @@ export default {
       let pageY = event.pageY
       this.mouse.x = pageX
       this.mouse.y = pageY
-      this.reflesh(pageX, pageY)
-
-      let canvasElm = document.getElementById('map-canvas')
-      const canvasX = canvasElm.getBoundingClientRect().left
-      const canvasY = canvasElm.getBoundingClientRect().top
-      const clientX = pageX - canvasX
-      const clientY = pageY - canvasY
-
-      // console.log(`マウス移動 page(${f(pageX)}, ${f(pageY)}), canvas(${f(canvasX)}, ${f(canvasY)}), client(${f(clientX)}, ${f(clientY)}) translateZ:${this.translateZ}`)
-      const gridObj = this.calcAddress(clientX, clientY)
-      const addressObj = {
-        c: gridObj.gridC - this.marginGridNum / 2,
-        r: gridObj.gridR - this.marginGridNum / 2
-      }
-      this.setMouseAddress(addressObj)
-      // console.log(`address(${addressObj.c}, ${addressObj.r}) grid(${gridObj.gridC}, ${gridObj.gridR}) marginGridNum:${this.marginGridNum}`)
+      this.reflesh()
     },
     wheel: function (delta) {
       const changeValue = 100
@@ -105,13 +97,133 @@ export default {
       }
       this.translateZ = afterTranslateZ
     },
+    leftDown: function () {
+      console.log('  [methods] mousedown left on GameTable')
+      this.mouse.left.from.x = this.mouse.x
+      this.mouse.left.from.y = this.mouse.y
+      this.isDraggingLeft = true
+    },
+    leftUp: function () {
+      console.log('  [methods] mouseup left on GameTable')
+      this.isDraggingLeft = false
+      this.mouse.left.totalX += this.mouse.left.draggingX
+      this.mouse.left.totalY += this.mouse.left.draggingY
+      this.mouse.left.draggingX = 0
+      this.mouse.left.draggingY = 0
+    },
+    rightDown: function () {
+      console.log('  [methods] mousedown right on GameTable')
+      this.mouse.right.from.x = this.mouse.x
+      this.mouse.right.from.y = this.mouse.y
+      this.angle.dragStart = this.calcCoordinate(this.mouse.x, this.mouse.y).angle
+      this.isDraggingRight = true
+    },
+    rightUp: function () {
+      console.log('  [methods] mouseup right on GameTable')
+      this.isDraggingRight = false
+      this.mouse.right.totalX += this.mouse.right.draggingX
+      this.mouse.right.totalY += this.mouse.right.draggingY
+      this.mouse.right.draggingX = 0
+      this.mouse.right.draggingY = 0
+
+      this.angle.total += Math.round(this.angle.dragging / 5) * 5
+      this.angle.dragging = 0
+    },
+    arrangeAngle: function (angle) {
+      if (angle > 180) { angle -= 360 }
+      if (angle < -180) { angle += 360 }
+      return angle
+    },
+    /**
+     * 指定されたスクリーン座標を元に、座標計算を行う
+     * @param  {Number} screenX スクリーン上のX座標
+     * @param  {Number} screenY スクリーン上のY座標
+     * @return {Object}         ソース参照
+     */
+    calcCoordinate: function (screenX, screenY) {
+      // スクロール倍率を考慮
+      const zoom = (1000 - this.translateZ) / 1000.0
+
+      // canvas上のマス座標を計算する
+      const cr = document.getElementById('map-canvas').getBoundingClientRect()
+      // canvasの中心点
+      const center = {
+        x: cr.x + cr.width / 2,
+        y: cr.y + cr.height / 2
+      }
+      // 中心座標を基準としたマウス座標
+      const loc = {
+        x: (screenX - center.x),
+        y: (screenY - center.y)
+      }
+
+      // 中心点と指定された座標とを結ぶ線の角度を求める
+      const angle = Math.atan2(loc.y, loc.x) * 180 / Math.PI
+
+      // 中心点と指定された座標とを結ぶ線の長さを求める
+      const distance = Math.sqrt(Math.pow(loc.x, 2) + Math.pow(loc.y, 2)) * zoom
+
+      // マップ回転前の角度を求める
+      const oldAngle = this.arrangeAngle(this.angle.total + this.angle.dragging)
+      const angleBeforeAround = this.arrangeAngle(angle - oldAngle)
+      const planeLocate = {
+        x: center.x + distance * Math.cos(angleBeforeAround * Math.PI / 180),
+        y: center.y + distance * Math.sin(angleBeforeAround * Math.PI / 180)
+      }
+
+      /*
+      const f = Math.floor
+      const planeLocateFromCenter = {
+        x: planeLocate.x - center.x,
+        y: planeLocate.y - center.y
+      }
+      console.log(
+          `zm:${zoom} `
+        + `center(${f(center.x)}, ${f(center.y)}) `
+        + `loc(${f(loc.x)}, ${f(loc.y)}) `
+        + `angle:${f(angle)} `
+        + `oldAngle:${f(oldAngle)} `
+        + `angleBeforeAround:${f(angleBeforeAround)} `
+        + `dist:${f(distance)} `
+        + `planeLocate(${f(planeLocate.x)}, ${f(planeLocate.y)}) `
+        + `planeLocateC(${f(planeLocateFromCenter.x)}, ${f(planeLocateFromCenter.y)}) `
+        )
+      */
+
+      const result = {
+        angle: angle, // 角度
+        planeLocate: planeLocate // マップ回転前のスクリーン座標
+      }
+      // console.log(`screen(${this.f(screenX)}, ${this.f(screenY)}), angle:${this.f(angle)}, distance:${this.f(distance)} plane(${this.f(planeLocate.x)}, ${this.f(planeLocate.y)})`)
+      return result
+    },
     f: function (v) {
       return Math.floor(v * 100) / 100
     },
-    calcAddress: function (clientX, clientY, offsetX = 0, offsetY = 0) {
-      // const f = this.f
+    calcAddress: function (screenX, screenY, offsetX = 0, offsetY = 0) {
+      // 回転やズームの前のスクリーン座標がどこになるかを計算し、そこをベースにマップ上の座標を算出する
+      let planeLocate = this.calcCoordinate(screenX, screenY).planeLocate
+      let canvasElm = document.getElementById('map-canvas')
+      const canvasRect = canvasElm.getBoundingClientRect()
+      const center = {
+        x: canvasRect.x + canvasRect.width / 2,
+        y: canvasRect.y + canvasRect.height / 2
+      }
+      const leftTop = {
+        x: center.x - this.gridC * this.gridSize / 2,
+        y: center.y - this.gridR * this.gridSize / 2
+      }
+      const clientX = planeLocate.x - leftTop.x
+      const clientY = planeLocate.y - leftTop.y
 
-      const zoom = (1000 - this.translateZ) / 1000
+      //
+      let gridC = Math.ceil(clientX / this.gridSize)
+      let gridR = Math.ceil(clientY / this.gridSize)
+
+      /*
+      const f = Math.floor
+      console.log(`screen(${f(screenX)}, ${f(screenY)}), center(${f(center.x)}, ${f(center.y)}), leftTop(${f(leftTop.x)}, ${f(leftTop.y)}), size(${f(this.sizeW)}, ${f(this.sizeH)}), planeLocate(${f(planeLocate.x)}, ${f(planeLocate.y)}), client(${f(clientX)}, ${f(clientY)})`)
+      */
 
       // ドロップ先のマス座標を算出
       let offsetGridX = offsetX / this.gridSize
@@ -128,8 +240,6 @@ export default {
       }
 
       // console.log(`client(${f(clientX)}, ${f(clientY)}) offset(${f(offsetX)}, ${f(offsetY)}), １マスのサイズ:${f(zoomedGridSize)} zoomed(${f(clientX / zoomedGridSize)}, ${f(clientY / zoomedGridSize)}) offsetGrid(${f(offsetGridX)}, ${f(offsetGridY)})`)
-      let gridC = Math.ceil(clientX * zoom / this.gridSize)
-      let gridR = Math.ceil(clientY * zoom / this.gridSize)
       if (offsetX !== undefined && offsetY !== undefined) {
         gridC -= offsetGridX
         gridR -= offsetGridY
@@ -142,19 +252,46 @@ export default {
         gridR: gridR
       }
     },
+    reflesh: function () {
+      if (this.isDraggingLeft) {
+        this.mouse.left.to.x = this.mouse.x
+        this.mouse.left.to.y = this.mouse.y
+        const moveLeftX = this.mouse.x - this.mouse.left.from.x
+        const moveLeftY = this.mouse.y - this.mouse.left.from.y
+        this.mouse.left.draggingX = moveLeftX
+        this.mouse.left.draggingY = moveLeftY
+      }
+
+      if (this.isDraggingRight) {
+        this.mouse.right.to.x = this.mouse.x
+        this.mouse.right.to.y = this.mouse.y
+        const moveRightX = this.mouse.x - this.mouse.right.from.x
+        const moveRightY = this.mouse.y - this.mouse.right.from.y
+        this.mouse.right.draggingX = moveRightX
+        this.mouse.right.draggingY = moveRightY
+
+        const angle = this.calcCoordinate(this.mouse.x, this.mouse.y).angle
+        let angleDiff = this.arrangeAngle(angle - this.angle.dragStart)
+        this.angle.dragging = angleDiff
+      }
+
+      // canvas上のマス座標を計算する
+      const gridObj = this.calcAddress(this.mouse.x, this.mouse.y)
+      const addressObj = {
+        c: gridObj.gridC,
+        r: gridObj.gridR
+      }
+      this.setMouseAddress(addressObj)
+    },
     drop: function (event) {
       // ドロップされた物の種類
       const kind = event.dataTransfer.getData('kind')
 
-      // マス座標
-      let canvasElm = document.getElementById('map-canvas')
-      const clientX = event.pageX - canvasElm.getBoundingClientRect().left
-      const clientY = event.pageY - canvasElm.getBoundingClientRect().top
-
       const offsetX = event.dataTransfer.getData('offsetX')
       const offsetY = event.dataTransfer.getData('offsetY')
 
-      const addressObj = this.calcAddress(clientX, clientY, offsetX, offsetY)
+      // マス座標
+      const addressObj = this.calcAddress(event.pageX, event.pageY, offsetX, offsetY)
       const gridC = addressObj.gridC
       const gridR = addressObj.gridR
 
@@ -187,46 +324,6 @@ export default {
           gridC: gridC
         })
       }
-    },
-    leftDown: function () {
-      console.log('  [methods] mousedown left on GameTable')
-      this.mouse.saveX = this.mouse.x
-      this.mouse.saveY = this.mouse.y
-      this.isDraggingLeft = true
-    },
-    leftUp: function () {
-      console.log('  [methods] mouseup left on GameTable')
-      this.isDraggingLeft = false
-      this.leftMove.totalX += this.leftMove.draggingX
-      this.leftMove.totalY += this.leftMove.draggingY
-      this.leftMove.draggingX = 0
-      this.leftMove.draggingY = 0
-    },
-    rightDown: function () {
-      console.log('  [methods] mousedown right on GameTable')
-      this.mouse.saveX = this.mouse.x
-      this.mouse.saveY = this.mouse.y
-      this.isDraggingRight = true
-    },
-    rightUp: function () {
-      console.log('  [methods] mouseup right on GameTable')
-      this.isDraggingRight = false
-      this.rightMove.totalX += this.rightMove.draggingX
-      this.rightMove.totalY += this.rightMove.draggingY
-      this.rightMove.draggingX = 0
-      this.rightMove.draggingY = 0
-    },
-    reflesh: function (x, y) {
-      const moveX = x - this.mouse.saveX
-      const moveY = y - this.mouse.saveY
-      if (this.isDraggingLeft) {
-        this.leftMove.draggingX = moveX
-        this.leftMove.draggingY = moveY
-      }
-      if (this.isDraggingRight) {
-        this.rightMove.draggingX = moveX
-        this.rightMove.draggingY = moveY
-      }
     }
   },
   computed: {
@@ -251,21 +348,20 @@ export default {
       const _this = this
       const translateZ = this.translateZ
       const zoom = (1000 - this.translateZ) / 1000
-      let translateX = this.leftMove.totalX + this.leftMove.draggingX
-      let translateY = this.leftMove.totalY + this.leftMove.draggingY
-      translateX *= zoom
-      translateY *= zoom
+      const totalLeftX = (this.mouse.left.totalX + this.mouse.left.draggingX) * zoom
+      const totalLeftY = (this.mouse.left.totalY + this.mouse.left.draggingY) * zoom
+      const rotateZ = this.arrangeAngle(this.angle.total + this.angle.dragging)
       // console.log(`rect(${this.sizeW}, ${this.sizeH}), position(${left}, ${top}), screen(${window.innerWidth}, ${window.innerHeight})`)
       return {
         width: this.sizeW + 'px',
         height: this.sizeH + 'px',
         transform:
           'translateZ(' + translateZ + 'px) ' +
-          'translateY(' + translateY + 'px) ' +
-          'translateX(' + translateX + 'px) ' +
+          'translateY(' + totalLeftY + 'px) ' +
+          'translateX(' + totalLeftX + 'px) ' +
           'rotateY(' + _this.rotateY + 'deg) ' +
           'rotateX(' + _this.rotateX + 'deg) ' +
-          'rotateZ(' + _this.rotateZ + 'deg)'
+          'rotateZ(' + rotateZ + 'deg)'
         /*
           ,
         background-image:
